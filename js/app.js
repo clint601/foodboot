1066cc72f0ddd50e320b4af3ab1d92a39369e458
clint601 <clintjackson601@gmail.com>
2024-12-09 15:03:32 -0600

    donefortheday

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ class Store {
             subtotal: 0
         }
 
-        this.menu ={
+        this.menu = {
 
             item1: {
                 id: 1,
@@ -87,10 +87,12 @@ class Store {
     init() {
         // console.log('initialized')
         this.loadItems()
+        this.addtoCart()
+        this.checkout()
     }
 
     loadItems() {
-        console.log('items loaded')
+        // console.log('items loaded')
         const itemDiv = document.getElementById('itemDiv')
 
         /**
@@ -112,11 +114,109 @@ class Store {
                         <span class="item-price" id="itemPrice">${item.price}</span>
                 </figcaption>
                 <p class="item-desc" id="itemDesc">${item.desc}</p>
-                <button class="btn menu text-capitalize" id="menuBtn" data-id=${item.id}>add to cart</button>
+                <button class="btn menu-btn text-capitalize" id="menuBtn" data-id=${item.id}>add to cart</button>
             </figure>
             `
             itemDiv.appendChild(product)
         }
+    }
+
+    addtoCart() {
+        const menuButtons = document.querySelectorAll('.menu-btn')
+        const cartItems = document.getElementById('cartItems')
+        const cartSubtotal = document.getElementById('cartSubtotal')
+        let price = 0
+
+        let subTimesQty = 0
+        const subtotalValue = document.getElementById('subtotalValue')
+        const taxValue = document.getElementById('taxValue')
+        let tax = 0
+        const deliveryValue = document.getElementById('deliveryValue')
+        const checkoutItemCount = document.getElementById('checkoutItemCount')
+        let deliveryFee = 6
+        let total = 0
+        let taxRate = .07
+        const totalValue = document.getElementById('totalValue')
+
+        // loop through this.menu
+        for (const key in this.menu) {
+            const item = this.menu[key]
+
+            // loop through buttons
+            menuButtons.forEach(button => {
+                button.addEventListener('click', ()=> {
+                    if (button.dataset['id'] == item.id) {
+                        this.itemsInCart.itemCount++
+                        price+= item.price
+                        this.itemsInCart.subtotal = price
+
+                        item.qty++
+
+                        subTimesQty = (item.price * item.qty).toFixed(2)
+                        tax = this.itemsInCart.subtotal * taxRate
+                        total = (this.itemsInCart.subtotal + tax + deliveryFee).toFixed(2)
+                    }
+
+                    // send to DOM
+                    cartItems.innerText = this.itemsInCart.itemCount
+                    cartSubtotal.innerText = price.toFixed(2)
+                    subtotalValue.innerText = this.itemsInCart.subtotal.toFixed(2)
+                    deliveryValue.innerText = deliveryFee.toFixed(2)
+                    taxValue.innerText = tax.toFixed(2)
+                    totalValue.innerText = total
+
+                    // if (this.itemsInCart.itemCount == 1) {
+                    //     checkoutItemCount.innerText = `${this.itemsInCart.itemCount} item`
+                    // } else {
+                    //     checkoutItemCount.innerText = `${this.itemsInCart.itemCount} items`
+                    // }
+
+                    checkoutItemCount.innerText = this.itemsInCart.itemCount == 1 ? `${this.itemsInCart.itemCount} item` : `${this.itemsInCart.itemCount} items`
+
+
+                })
+            })
+        }
+
+    }
+
+    checkout() {
+        const cartBtn = document.getElementById('cartBtn')
+        const checkoutPage = document.getElementById('checkoutPage')
+        const menuSection = document.getElementById('menuSection')
+        const tableBody = document.getElementById('tbody')
+
+        let subTimesQty = 0
+
+        cartBtn.addEventListener('click', ()=> {
+            // console.log('click')
+            if (menuSection.classList.contains('d-none')) return
+
+            checkoutPage.classList.remove('d-none')
+            menuSection.classList.add('d-none')
+
+            for (const key in this.menu) {
+                const item = this.menu[key]
+
+                if (item.qty > 0) {
+                    subTimesQty = (item.qty * item.price).toFixed(2)
+
+                    const tableRow = document.createElement('tr')
+                    tableRow.className = 'item-checkout'
+
+                    tableBody.innerHTML+= `
+                        <td id="itemImg">
+                            <img src="${item.img}" alt="${item.alt}" class="img-fluid item-img" />
+                        </td>
+                        <td class="unit-price">${item.price.toFixed(2)}</td>
+                        <td class="item-quantity">${item.qty}</td>
+                        <td class="item-subtotal">${subTimesQty}</td>
+                    `
+
+                    tableBody.appendChild(tableRow)
+                }
+            }
+        })
     }
 }
 
